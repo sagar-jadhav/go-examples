30dbfdd7581d0dbe26faeec66e6ab47b0955a9b1
Luis Herrera <luhegoes@gmail.com>
2019-10-05 01:53:34 -0500

    Add manipulate pointer inside function example

--- src/pointer.go
+++ src/pointer.go
@@ -18,5 +18,13 @@ func main() {
    *size = 85 
    fmt.Println("New size value is", *size)
 
+   // Pass a pointer to a function so we can manipulate the value inside the function
+   var p = 0
+   fmt.Printf("Original value  for the variable p: %d\n", p)
+   manipulatePointer(&p)
+   fmt.Printf("The value of the variable p has been updated inside the function: %d\n", p)
+}
 
+func manipulatePointer(p *int) {
+   *p++;
 }
