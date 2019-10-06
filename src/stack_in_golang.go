0e32f658afc2f568e58cc38077cb19a992a9c3e9
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2019-10-06 11:25:22 +0530

    Add comments to explain

--- src/stack_in_golang.go
+++ src/stack_in_golang.go
@@ -1,45 +1,70 @@
 package main
-import "fmt"
-type Stack interface{
-	push(item int)
-	pop()int 
-	peek()
-	isEmpty()bool
-} 
 
-func (s *stack)isEmpty()bool{
-	if s.head == nil{
+import "fmt"
+
+// Stack is the Abstract Data type following FIFO (First In First Out) principle which has certain functionalities like-
+// (a) push- Insert new data at one end
+// (b) pop- Remove (and return) data from the end insertion has happened
+// (c) peek- Return the data from the end insertion has happened (without removal)
+
+// There are many real-life examples of a stack. Consider an example of plates stacked over one
+// another in the canteen. The plate which is at the top is the first one to be removed, i.e. the plate
+// which has been placed at the bottommost position remains in the stack for the longest period of time. So, it
+// can be simply seen to follow LIFO(Last In First Out)/FILO(First In Last Out) order.
+
+// Since Interface declares the functionalities of the type that defines it,
+// declaring the above mentioned methods in the interfaces essentially decalres
+// a Stack ADT.
+// Any data type which implements the Stack interface becomes the Stack. it can
+// be an array, a Linked List, etc. In this case we are implementing Stack on a Linked List.
+
+type Stack interface {
+	push(item int)
+	pop() int
+	peek()
+	isEmpty() bool
+}
+
+// Check if stack is empty
+func (s *stack) isEmpty() bool {
+	if s.head == nil {
 		return true
 	}
 	return false
 }
-type node struct{
-	data int 
+
+// Node of the linked list
+type node struct {
+	data int
 	next *node
 }
-type stack struct{
+
+// Stack always stores the address of the head node
+type stack struct {
 	head *node
 }
-func (s *stack)push(item int){
+
+// push new element at one end
+func (s *stack) push(item int) {
 	newNode := node{
 		data: item,
 		next: nil,
 	}
-	if s.head == nil{
+	if s.head == nil {
 		s.head = &newNode
-	}else{
+	} else {
 		temp := s.head
 		newNode.next = temp
 		s.head = &newNode
 	}
-	//printStack(s)
-	fmt.Printf("PUSHED: %d\n",newNode.data)
+	fmt.Printf("PUSHED: %d\n", newNode.data)
 	printStack(s)
-	return 
+	return
 }
 
-func (s *stack)pop()int{
-	if s.isEmpty(){
+// remove and return the element from the same end as insertion
+func (s *stack) pop() int {
+	if s.isEmpty() {
 		fmt.Println("STACK UNDERFLOW!")
 		return 0
 	}
@@ -48,41 +73,43 @@ func (s *stack)pop()int{
 	return temp.data
 }
 
-func (s *stack)peek(){
+// peek the top of the stack
+func (s *stack) peek() {
 	temp := s.head
-	for temp.next != nil{
+	for temp.next != nil {
 		temp = temp.next
 	}
 	fmt.Println(temp.data)
 }
 
-func printStack(s *stack){
-	if s.head == nil{
+func printStack(s *stack) {
+	if s.head == nil {
 		return
 	}
 	temp := s.head
-	for temp != nil{
+	for temp != nil {
 		fmt.Println(temp.data)
 		temp = temp.next
 	}
 }
 
-func new()Stack{
+// constructor for new node
+func new() Stack {
 	return &stack{
 		head: nil,
 	}
 }
-func main(){
+func main() {
 	st := new()
 	st.push(1)
 	st.push(2)
 	st.push(3)
 	st.push(4)
 	st.push(5)
-	fmt.Println("POP:",st.pop())
-	fmt.Println("POP:",st.pop())
-	fmt.Println("POP:",st.pop())
-	fmt.Println("POP:",st.pop())
-	fmt.Println("POP:",st.pop())
-	fmt.Println("POP:",st.pop())
+	fmt.Println("POP:", st.pop())
+	fmt.Println("POP:", st.pop())
+	fmt.Println("POP:", st.pop())
+	fmt.Println("POP:", st.pop())
+	fmt.Println("POP:", st.pop())
+	fmt.Println("POP:", st.pop())
 }
