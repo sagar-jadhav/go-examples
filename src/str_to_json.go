09b903965ce5487da85450d94ae872dff684517a
yuvaraj_11 <49576526+yuvan11@users.noreply.github.com>
2021-10-09 11:42:17 +0530

    Resolves #147 (#153)
    * Resolves #147
    
    * Fixed typo

---
+++ src/str_to_json.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Laptop has neccessary fields
+type Laptop struct {
+	ID          int64   `json:"id"`
+	Model       string  `json:"model"`
+	Color       string  `json:"color"`
+	Weight      float32 `json:"weight"`
+	HealthCheck bool    `json:"healthcheck"`
+}
+
+// laptopJson assigns JSON string
+func main() {
+	laptopJson := `{
+      "id" : 1000001 ,
+      "model": "Asus Rog",
+      "color" : "Black",
+      "weight": 1.90,
+      "healthcheck":true}`
+
+	var laptopObj Laptop
+
+	/* JSON string to JSON object conversion
+	   After conversion, laptop stores the result
+	*/
+	err := json.Unmarshal([]byte(laptopJson), &laptopObj)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("JSON string to JSON object conversion:")
+	fmt.Printf("Laptop ID: %d , Model: %s, Color: %s, Weight: %g, HealthCheck: %t \n",
+		laptopObj.ID, laptopObj.Model, laptopObj.Color, laptopObj.Weight, laptopObj.HealthCheck)
+
+	/* JSON object to JSON string conversion
+	   laptopString holds the  JsonString which obtained after marshalling JSON object
+	*/
+	laptopString, _ := json.Marshal(laptopObj)
+	fmt.Println("JSON object to JSON string conversion")
+	fmt.Println(string(laptopString))
+
+}
